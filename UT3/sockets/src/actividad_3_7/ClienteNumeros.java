d5a37853129057d77cb45a8a017053c48b86cd5c
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-01-22 12:32:18 +0100

    [UT3] SOCKETS: Ejercicio 7 (copiada solución de clase para tener ambas)

--- UT3/sockets/src/actividad_3_7/ClienteNumeros.java
+++ UT3/sockets/src/actividad_3_7/ClienteNumeros.java
@@ -3,10 +3,64 @@ package actividad_3_7;
 import util.Teclado;
 
 import java.io.*;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.util.InputMismatchException;
 
 public class ClienteNumeros {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        String host = "localhost";
+        int port = 6000; // puerto remoto
+
+        Socket client = null;
+
+        try {
+            client = new Socket(host, port);
+            System.out.println("PROGRAMA CLIENTE INICIADO...");
+        } catch (ConnectException ex) {
+            System.out.println("ERROR AL ESTABLECER LA CONEXIÓN CON EL SERVIDOR...");
+            System.exit(0);
+        }
+
+        // Flujo de entrada para objetos
+        ObjectInputStream numIn = new ObjectInputStream(client.getInputStream());
+
+        // Flujo de salida para objetos
+        ObjectOutputStream numOut = new ObjectOutputStream(client.getOutputStream());
+
+        int number = 1;
+
+        do {
+            try {
+                number = Teclado.getNum("Número: ");
+            } catch (InputMismatchException ex) {
+                number = 1; // se asigna un valor mayor a 0 para poder pedir otro número
+                System.out.println("\nNúmero incorrecto...\n");
+                continue;
+            }
+            Numeros numeros = new Numeros();
+            numeros.setNumero(number);
+
+            // Se envía el objeto
+            numOut.writeObject(numeros);
+
+            // Se recibe un objeto
+            if (number > 0) {
+                Numeros num = (Numeros) numIn.readObject(); // recibo objeto
+
+                System.out.println("Cuadrado: " + num.getCuadrado() + ", Cubo: " + num.getCubo());
+            }
+        } while (number > 0);
+
+        System.out.println("CLIENTE FINALIZADO...");
+
+        // Cerrar Streams y Sockets
+        numIn.close();
+        numOut.close();
+        client.close();
+
+        /* -- MI SOLUCIÓN --
         String host = "localhost";
         int puerto = 6000;
 
@@ -31,5 +85,6 @@ public class ClienteNumeros {
         }
 
         cliente.close();
+        */
     }
 }
--- UT3/sockets/src/actividad_3_7/ServidorNumeros.java
+++ UT3/sockets/src/actividad_3_7/ServidorNumeros.java
@@ -7,9 +7,57 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class ServidorNumeros {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        int port = 6000; // puerto
+        ServerSocket server = new ServerSocket(port);
+
+        System.out.println("Esperando al cliente...");
+        Socket client = server.accept();
+
+        // Se prepara un flujo de salida para objetos
+        ObjectOutputStream outNum = new ObjectOutputStream(client.getOutputStream());
+
+        // Se obtiene un Stream para leer objetos
+        ObjectInputStream inNum = new ObjectInputStream(client.getInputStream());
+
+        Numeros num = new Numeros();
+
+        try {
+            num = (Numeros) inNum.readObject();
+        } catch (SocketException ex) {
+            System.out.println("ERROR AL RECIBIR DATOS DEL CLIENTE... " + ex.getMessage());
+            System.exit(0);
+        }
+
+        while (num.getNumero() > 0) {
+            num.setCuadrado(Calculadora.calcularCuadrado(num.getNumero()));
+            num.setCubo(Calculadora.calcularCubo(num.getNumero()));
+
+            System.out.println("Recibo: " + num.getNumero());
+            outNum.writeObject(num); // enviando objeto
+
+            try {
+                num = (Numeros) inNum.readObject();
+            } catch (SocketException ex) {
+                System.out.println("ERROR AL RECIBIR DATOS DEL CLIENTE... " + ex.getMessage());
+                System.exit(0);
+            }
+        }
+
+        System.out.println("SERVIDOR FINALIZADO...");
+
+        // Cerrar Streams y Sockets
+        outNum.close();
+        inNum.close();
+        client.close();
+        server.close();
+
+        /* -- MI SOLUCIÓN --
+
         int puerto = 6000;
         ServerSocket servidor = new ServerSocket(puerto);
 
@@ -35,5 +83,7 @@ public class ServidorNumeros {
         }
         cliente.close();
         servidor.close();
+
+        */
     }
 }
