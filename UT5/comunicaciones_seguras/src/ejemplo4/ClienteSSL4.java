2428c4934d2839dc475d2a48c2b2474e9815d6b5
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-02-27 10:40:25 +0100

    UT5 - Comunicaciones Seguras - Ejemplo 4 (estructura, no realizado)

---
+++ UT5/comunicaciones_seguras/src/ejemplo4/ClienteSSL4.java
@@ -0,0 +1,72 @@
+package ejemplo4;
+
+import javax.net.ssl.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.math.BigInteger;
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+
+public class ClienteSSL4 {
+    public static void main(String[] args) throws Exception {
+        String host = "localhost";
+        int port = 6000;
+
+        System.out.println("PROGRAMA CLIENTE INICIADO...");
+
+        // Definir el fichero almacén que contiene el certificado de confianza y la clave para acceder a él
+        FileInputStream ficCerConf = new FileInputStream("CliCertConfianza");
+        String claveCerConf = "890123";
+
+        // Cargar en un KeyStore el almacén de certificados de confianza
+        KeyStore almacenConf = KeyStore.getInstance(KeyStore.getDefaultType());
+        almacenConf.load(ficCerConf, claveCerConf.toCharArray());
+
+        // Crear el gestor de confianza a partir del objeto KeyStore e inicializarlo con la clave del almacén
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(almacenConf);
+
+        // Creación del contexto con soporte TLS
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+
+        // Creación del socket SSL de cliente a partir del contexto
+        SSLSocketFactory socketFactory = sslContext.getSocketFactory();
+        SSLSocket client = (SSLSocket) socketFactory.createSocket(host, port);
+
+        // Creo flujo de salida al servidor
+        DataOutputStream outputStream = new DataOutputStream(client.getOutputStream());
+
+        // Envío un saludo al servidor
+        outputStream.writeUTF("Saludos al SERVIDOR desde el CLIENTE");
+
+        // Creo un flujo de entrada al servidor
+        DataInputStream inputStream = new DataInputStream(client.getInputStream());
+
+        // El servidor me envía un mensaje
+        System.out.println("Recibiendo del SERVIDOR: \n\t" + inputStream.readUTF());
+
+        // Información SSL
+        SSLSession session = client.getSession();
+        X509Certificate certificate = (X509Certificate) session.getPeerCertificates()[0];
+        System.out.println(
+                        "Host                   : " + session.getPeerHost() + "\n" +
+                        "Cifrado                : " + session.getCipherSuite() + "\n" +
+                        "Protocolo              : " + session.getProtocol() + "\n" +
+                        "IDentificador          : " + new BigInteger(session.getId()) + "\n" +
+                        "Creación de la sesión  : " + session.getCreationTime() + "\n" +
+                        "Propietario            : " + certificate.getSubjectDN() + "\n" +
+                        "Algoritmo              : " + certificate.getSigAlgName() + "\n" +
+                        "Tipo                   : " + certificate.getType() + "\n" +
+                        "Emisor                 : " + certificate.getIssuerDN() + "\n" +
+                        "Número de serie        : " + certificate.getSerialNumber()
+        );
+
+
+        // Cerrar Streams y Sockets
+        inputStream.close();
+        outputStream.close();
+        client.close();
+    }
+}
---
+++ UT5/comunicaciones_seguras/src/ejemplo4/ServidorSSL4.java
@@ -0,0 +1,56 @@
+package ejemplo4;
+
+import javax.net.ssl.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+
+public class ServidorSSL4 {
+    public static void main(String[] args) throws Exception {
+        int port = 6000;
+
+        // Definir el fichero almacén que contiene el certificado y la clave para acceder a él
+        FileInputStream ficAlmacen = new FileInputStream("AlmacenSrv");
+        String claveAlmacen = "1234567";
+
+        // Cargar en un KeyStore el almacén que contiene el certificado
+        KeyStore almacen = KeyStore.getInstance(KeyStore.getDefaultType());
+        almacen.load(ficAlmacen, claveAlmacen.toCharArray());
+
+        // Crear el gestor de claves a partir del objeto KeyStore e inicializarlo con la clave del almacén
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        keyManagerFactory.init(almacen, claveAlmacen.toCharArray());
+
+        // Creación del contexto con soporte TLS
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+
+        // Creación del socket SSL de servidor a partir del contexto
+        SSLServerSocketFactory serverSocket = sslContext.getServerSocketFactory();
+        SSLServerSocket sslServer = (SSLServerSocket) serverSocket.createServerSocket(port);
+
+        SSLSocket clientConnected = null;
+        DataInputStream inputStream = null; // Flujo de entrada del cliente
+        DataOutputStream outputStream = null; // Flujo de salida al cliente
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Esperando al cliente " + i);
+            clientConnected = (SSLSocket) sslServer.accept();
+
+            // El cliente me envía un mensaje
+            inputStream = new DataInputStream(clientConnected.getInputStream());
+            System.out.println("Recibiendo del CLIENTE: " + i + "\n\t" + inputStream.readUTF());
+
+            // Envío un saludo al cliente
+            outputStream = new DataOutputStream(clientConnected.getOutputStream());
+            outputStream.writeUTF("Saludos al cliente del servidor");
+        }
+
+        // Cerrar Streams y Sockets
+        inputStream.close();
+        outputStream.close();
+        clientConnected.close();
+        sslServer.close();
+    }
+}
